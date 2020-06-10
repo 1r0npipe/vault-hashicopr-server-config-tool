27a27b61912c62cfdb7fa5760ceec7b5e0758c59
Evgeny Makhinya <eugene.makhinya@gmail.com>
2020-06-10 17:18:09 +0100

    established data structures

--- buildpki.py
+++ buildpki.py
@@ -1,6 +1,9 @@
 import argparse
 import sys
 
+data = dict()
+arr_data = []
+
 arg_parser = argparse.ArgumentParser(
     description='This tool is generating the CAs based on the config file, you can use also dry-run option')
 
@@ -10,4 +13,16 @@ arg_parser.add_argument('--dry-run', action='store_true', required=False, defaul
                         help='please use it just to simulate the run')
 args = arg_parser.parse_args()
 
-print(args)
+try:
+    try:
+        cert_file = open(args.config, 'r')
+        for line in cert_file.readlines():
+            data['path'], data['domain'], data['issuer'], data['hours'] = line.split(
+                '|')
+            arr_data.append(data)
+        print(arr_data)
+    finally:
+        cert_file.close()
+except:
+    print("The file cannot be opened or something goes worng, please check the file")
+    exit()
