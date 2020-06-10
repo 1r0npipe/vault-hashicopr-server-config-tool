4912dd11d886ab8f52d069740d9e70654d3ae7a0
Evgeny Makhinya <eugene.makhinya@gmail.com>
2020-06-10 16:43:30 +0100

    check arguments options - works

---
+++ buildpki.py
@@ -0,0 +1,13 @@
+import argparse
+import sys
+
+arg_parser = argparse.ArgumentParser(
+    description='This tool is generating the CAs based on the config file, you can use also dry-run option')
+
+arg_parser.add_argument('-config', metavar='path_to_conf', type=str, required=True,
+                        help='please use the path to the correct config file')
+arg_parser.add_argument('--dry-run', action='store_true', required=False, default=False,
+                        help='please use it just to simulate the run')
+args = arg_parser.parse_args()
+
+print(args)
