fb4c874640dcb3c63bf503c195eafdf51b754bbd
Evgeny Makhinya <eugene.makhinya@gmail.com>
2020-06-10 20:06:48 +0100

    added check_endpoint function

--- buildpki.py
+++ buildpki.py
@@ -1,11 +1,24 @@
 import argparse
-
+import requests
 import sys
 
 url_issuer = "http://127.0.0.1:8200"
 data = dict()
 arr_data = list()
 
+
+def check_endpoint(url):
+    SUCCESS_CODE = 200
+    try:
+        request = requests.get(url)
+    except:
+        return False
+    else:
+        if request.status_code == SUCCESS_CODE:
+            return True
+
+
+# setting up the argument parser enities to apply
 arg_parser = argparse.ArgumentParser(
     description='This tool is generating the CAs based on the config file, you can use also dry-run option')
 
@@ -15,6 +28,7 @@ arg_parser.add_argument('--dry-run', action='store_true', required=False, defaul
                         help='please use it just to simulate the run')
 args = arg_parser.parse_args()
 
+# reading the content of file
 try:
     try:
         cert_file = open(args.config, 'r')
@@ -29,11 +43,12 @@ except:
     print("The file cannot be opened or something goes worng, please check the file")
     exit()
 
+# checking if dry-run is required, afterwards the EXIT will happen
 if args.dry_run:
-    print("By using dry-run option we will be issuing this certificates for those autorieties:")
+    print("By using dry-run option, we will be issuing this certificates for those autorieties, without any real modifications:")
     for elem in arr_data:
         print("Mounting {} for {} with issuer {} by TTL: {}".format(
             elem['path'], elem['domain'], elem['issuer'], elem['hours']))
     exit()
 
-print(arr_data)
+print(check_endpoint(url_issuer))
